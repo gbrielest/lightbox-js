14d925e7e85359ecd65ff15e6438b9fff4c6fb2f
Gabriele Stefani Teixeira <87157629+gbrielest@users.noreply.github.com>
2021-10-23 08:04:23 -0300

    lightbox-js
    exemplo funcional utilizando o plugin jQuery Magnific Popup ( Magnific Popup | jQuery Plugin Registry )

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.link-foto').magnificPopup({
+        type:'image'
+    });
+
+    /* $('.link-video').magnificPopup({
+        type:'iframe'
+    });*/
+
+  });
